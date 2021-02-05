f40b25b93d8129f82d891df9079cc38d96ed4dde
Harman Kahlon <kahlon.harmandeep@codenation.co.in>
2021-02-05 15:40:47 +0530

    Fix indent
    Signed-off-by: Harman Kahlon <kahlon.harmandeep@codenation.co.in>
    Change-Id: I1a7d3da892ff025bef07d42454ef7ca26586bd05

--- toggle-lock.py
+++ toggle-lock.py
@@ -1,5 +1,4 @@
 import subprocess
-import sys
 import uuid
 from namespaces import namespaces
 import xml.etree.ElementTree as ET
@@ -11,33 +10,31 @@ def run(command, dir='.'):
         print(output)
         print(error)
 
-filename = sys.argv[1]
-section_index = sys.argv[2]
+def toggle_lock(filename, section_index):
+    tmp_dir = uuid.uuid4().hex
+    unzip_cmd = "unzip " + filename + " -d " + tmp_dir
+    run(unzip_cmd)
 
-tmp_dir = uuid.uuid4().hex
-unzip_cmd = "unzip " + filename + " -d " + tmp_dir
-run(unzip_cmd)
+    tree = ET.parse(tmp_dir + "/content.xml")
+    root = tree.getroot()
 
-tree = ET.parse(tmp_dir + "/content.xml")
-root = tree.getroot()
+    sections = root.findall('.//text:section', namespaces)
+    # print(ET.tostring(sections[int(section_index)-1], encoding="utf-8", method="xml"))
+    section = sections[int(section_index) - 1]
+    if section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] == "true":
+        section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] = "false"
+    else:
+        section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] = "true"
 
-sections = root.findall('.//text:section', namespaces)
-# print(ET.tostring(sections[int(section_index)-1], encoding="utf-8", method="xml"))
-section = sections[int(section_index) - 1]
-if section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] == "true":
-    section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] = "false"
-else:
-    section.attrib["{0}protected".format("{" + namespaces["text"] + "}")] = "true"
+    content_file = open(tmp_dir + "/content.xml", "wb")
+    data = ET.tostring(root, encoding='UTF-8', method='xml')
+    content_file.write(data)
 
-content_file = open(tmp_dir + "/content.xml", "wb")
-data = ET.tostring(root, encoding='UTF-8', method='xml')
-content_file.write(data)
+    zip_cmd = "zip -r " + filename + " ."
+    run(zip_cmd, tmp_dir)
 
-zip_cmd = "zip -r " + filename + " ."
-run(zip_cmd, tmp_dir)
+    mv_cmd = "mv " + tmp_dir + "/" + filename + " ."
+    run(mv_cmd)
 
-mv_cmd = "mv " + tmp_dir + "/" + filename + " ."
-run(mv_cmd)
-
-rm_cmd = "rm -rf " + tmp_dir
-run(rm_cmd)
+    rm_cmd = "rm -rf " + tmp_dir
+    run(rm_cmd)
